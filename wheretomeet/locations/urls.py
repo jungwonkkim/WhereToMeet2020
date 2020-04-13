acf481823cec2db5c9d978574812ba73a54712c8
김정원 <jungwonkkim@gmail.com>
2020-04-13 22:06:02 +0900

    search.html

--- wheretomeet/locations/urls.py
+++ wheretomeet/locations/urls.py
@@ -5,5 +5,6 @@ app_name = "locations"
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('result/', views.result, name='result')
+    path('result/', views.result, name='result'),
+    path('<int:people>/', views.search, name='search'),
 ]
--- wheretomeet/locations/views.py
+++ wheretomeet/locations/views.py
@@ -1,9 +1,21 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.http import require_POST
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'locations/index.html')
+    if request.method  =="POST":
+        people = request.POST.get('people')
+        return redirect('locations:search', people)
+    else:
+        return render(request, 'locations/index.html')
 
 def result(request):
+    rest_api_key = '70b3da71d4666c940fd8657467651b15'
     pass
+
+def search(request,people):
+    context={
+        'people': people
+    }
+    return render(request, 'locations/search.html', context)
